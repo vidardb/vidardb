8b1f596165aa3cef189a30f7cc4fffb206619b92
chenquanzhao <quanzhao.cqz@gmail.com>
2020-02-04 14:42:22 +0800

    release vidardb-0.1.0

--- include/vidardb/version.h
+++ include/vidardb/version.h
@@ -1,11 +1,16 @@
+// Copyright (c) 2020-present, VidarDB, Inc.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+//
 // Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree. An additional grant
 // of patent rights can be found in the PATENTS file in the same directory.
 #pragma once
 
-#define VIDARDB_MAJOR 4
-#define VIDARDB_MINOR 9
+#define VIDARDB_MAJOR 0
+#define VIDARDB_MINOR 1
 #define VIDARDB_PATCH 0
 
 // Do not use these. We made the mistake of declaring macros starting with
--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* vidardb_build_git_sha = "vidardb_build_git_sha:5e4b7bc999620eace975d6e4d260750c16208212";
-const char* vidardb_build_git_date = "vidardb_build_git_date:2020-02-03";
+const char* vidardb_build_git_sha = "vidardb_build_git_sha:eac4721f68558b89189bcfb353915396dbf956ea";
+const char* vidardb_build_git_date = "vidardb_build_git_date:2020-02-04";
 const char* vidardb_build_compile_date = __DATE__;
