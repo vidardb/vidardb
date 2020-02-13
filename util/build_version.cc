eac6418dd5b880c2c7288086acf2de519459ab38
chenquanzhao <quanzhao.cqz@gmail.com>
2020-02-14 00:15:58 +0800

    update adaptive_table_factory map to unordered_map

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* vidardb_build_git_sha = "vidardb_build_git_sha:90611a0f50f8e96f728bfc886b68fcdaf455a36b";
-const char* vidardb_build_git_date = "vidardb_build_git_date:2020-02-08";
+const char* vidardb_build_git_sha = "vidardb_build_git_sha:a1736b48aa8bf076e1e5c7c14ab3f46d7da52590";
+const char* vidardb_build_git_date = "vidardb_build_git_date:2020-02-14";
 const char* vidardb_build_compile_date = __DATE__;
