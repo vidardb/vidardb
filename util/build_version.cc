ede610cb98cb38c872544abd4aebba3fd0de1316
chenquanzhao <quanzhao.cqz@gmail.com>
2020-03-22 11:28:56 +0800

    Use variable length prefix for encoding splitter

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* vidardb_build_git_sha = "vidardb_build_git_sha:d34de0d478b98b736b42675de8fe970c677bcd86";
-const char* vidardb_build_git_date = "vidardb_build_git_date:2020-03-19";
+const char* vidardb_build_git_sha = "vidardb_build_git_sha:c37d3201bf5d46bd9d887237eaca467c329f642c";
+const char* vidardb_build_git_date = "vidardb_build_git_date:2020-03-22";
 const char* vidardb_build_compile_date = __DATE__;
