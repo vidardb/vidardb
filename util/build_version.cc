33d2bf7960b150666d14d7159ed64780e9e554b3
chenquanzhao <quanzhao.cqz@gmail.com>
2020-04-11 21:28:31 +0800

    Fix test build error

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,5 +1,5 @@
 #include "build_version.h"
 const char* vidardb_build_git_sha =
-    "vidardb_build_git_sha:2a30f853f5a4ee2d2a271ed82a535e9ab3102142";
+    "vidardb_build_git_sha:5cca97c1f4a668c1bc621e6310d4f322eb16b747";
 const char* vidardb_build_git_date = "vidardb_build_git_date:2020-04-11";
 const char* vidardb_build_compile_date = __DATE__;
