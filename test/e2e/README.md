08ad826dcf386e4183852632a2eff7c1e7aead38
chenquanzhao <quanzhao.cqz@gmail.com>
2020-04-19 10:17:55 +0800

    Update e2e test makefile and add README.md

---
+++ test/e2e/README.md
@@ -0,0 +1,7 @@
+- Run all VidarDB e2e tests:
+
+    ```shell
+    # run it in the 'test/e2e' directory
+
+    make e2e-test
+    ```
--- util/build_version.cc
+++ util/build_version.cc
@@ -1,5 +1,5 @@
 #include "build_version.h"
 const char* vidardb_build_git_sha =
-    "vidardb_build_git_sha:18db566669f3abf0a0c8ef7812f47c924ffafda6";
-const char* vidardb_build_git_date = "vidardb_build_git_date:2020-04-12";
+    "vidardb_build_git_sha:e6a59a774dcd8167b4436ca6b5d0b76c0a8b515c";
+const char* vidardb_build_git_date = "vidardb_build_git_date:2020-04-19";
 const char* vidardb_build_compile_date = __DATE__;
