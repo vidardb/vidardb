bf56f0b578ccefb4281a86766756f267560524c2
chenquanzhao <quanzhao.cqz@gmail.com>
2020-05-27 01:33:47 +0800

    Release v0.1.3

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,5 +1,5 @@
 #include "build_version.h"
 const char* vidardb_build_git_sha =
-    "vidardb_build_git_sha:340a47e4478539a97f6cff7cf115a39e23378ce0";
-const char* vidardb_build_git_date = "vidardb_build_git_date:2020-05-25";
+    "vidardb_build_git_sha:a91777fadd635fcc61caf5074a6e7cfc9b9a4ad0";
+const char* vidardb_build_git_date = "vidardb_build_git_date:2020-05-27";
 const char* vidardb_build_compile_date = __DATE__;
