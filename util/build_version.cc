33c0bd982d53fe26bc2a0e3da58ef3db3e7ef945
chenquanzhao <quanzhao.cqz@gmail.com>
2020-06-05 23:07:28 +0800

    Resolve lite e2e test error

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* vidardb_build_git_sha = "vidardb_build_git_sha:20146190ad62afd266f30aeb87a40d1756635eec";
+const char* vidardb_build_git_sha = "vidardb_build_git_sha:55d00bcb4d90eca0c75ac65188c2595b2a51c206";
 const char* vidardb_build_git_date = "vidardb_build_git_date:2020-06-05";
 const char* vidardb_build_compile_date = __DATE__;
