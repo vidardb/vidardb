d5a068a83a897cc5fc8c658ba2c05bd2a2b6bd0c
chenquanzhao <quanzhao.cqz@gmail.com>
2020-06-06 23:12:16 +0800

    Fix e2e test compile fail

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* vidardb_build_git_sha = "vidardb_build_git_sha:142228c2dbd6383669abfeacc93d79a2bfb015c7";
+const char* vidardb_build_git_sha = "vidardb_build_git_sha:c503685baf92c86a0d1446e2d85a9ec28a35c52e";
 const char* vidardb_build_git_date = "vidardb_build_git_date:2020-06-06";
 const char* vidardb_build_compile_date = __DATE__;
