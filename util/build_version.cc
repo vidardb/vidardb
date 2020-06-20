7c116c7f1daa0649b2580ab55569255f827422ea
shichao <jsc0218@gmail.com>
2020-06-20 15:34:27 -0400

    add back and modify transaction interface

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,5 +1,5 @@
 #include "build_version.h"
 const char* vidardb_build_git_sha =
-    "vidardb_build_git_sha:d48d66ad3626e6e4977c3a28383be3ab5876c46c";
-const char* vidardb_build_git_date = "vidardb_build_git_date:2020-06-19";
+    "vidardb_build_git_sha:cc03053e53893428c26624b8ce844dc8ddaf8161";
+const char* vidardb_build_git_date = "vidardb_build_git_date:2020-06-20";
 const char* vidardb_build_compile_date = __DATE__;
