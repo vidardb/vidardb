90580b166e56a46ed12b017f5c50362c30919ae0
shichao <jsc0218@gmail.com>
2020-06-21 22:13:09 -0400

    fix the example bug

--- util/build_version.cc
+++ util/build_version.cc
@@ -1,5 +1,5 @@
 #include "build_version.h"
 const char* vidardb_build_git_sha =
-    "vidardb_build_git_sha:fb3f1a3b30606f1e1897ac846395aeab5bfcbee9";
+    "vidardb_build_git_sha:baa7d970fd42994e36112c9b8f9f8d2687d5bf55";
 const char* vidardb_build_git_date = "vidardb_build_git_date:2020-06-21";
 const char* vidardb_build_compile_date = __DATE__;
