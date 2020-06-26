9c4d7f3857ae7468cf98cb6458a68c69b068cbde
Weitian Xing <xingweitian@gmail.com>
2020-06-26 19:38:50 -0400

    Adapt the code to work with cmake. (#29)
    * Adapt the code to work with cmake.
    
    * Revert the change to build_version.cc, remove the dependency of build_version.cc.in in CMakeLists.txt.
    Unify to use build_version.cc.
    
    * Fix typo.

--- build_tools/version.sh
+++ build_tools/version.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 if [ "$#" = "0" ]; then
-  echo "Usage: $0 major|minor|patch"
+  echo "Usage: $0 major|minor|patch|full"
   exit 1
 fi
+
 if [ "$1" = "major" ]; then
   cat include/vidardb/version.h  | grep MAJOR | head -n1 | awk '{print $3}'
 fi
@@ -11,4 +12,11 @@ if [ "$1" = "minor" ]; then
 fi
 if [ "$1" = "patch" ]; then
   cat include/vidardb/version.h  | grep PATCH | head -n1 | awk '{print $3}'
+fi
+if [ "$1" = "full" ]; then
+  awk '/#define VIDARDB/ { env[$2] = $3 }
+       END { printf "%s.%s.%s\n", env["VIDARDB_MAJOR"],
+                                  env["VIDARDB_MINOR"],
+                                  env["VIDARDB_PATCH"] }'  \
+      include/vidardb/version.h
 fi
